5d7b84b63abc035282239860a1c9786e9286f808
Noman <noman-shahid@hotmail.com>
2017-08-28 21:13:27 -0400

    Completed preliminary screening process, added dummy stock data to work with

--- src/Main.scala
+++ src/Main.scala
@@ -1,13 +1,31 @@
 import java.io.PrintStream
 import scala.io.Source
 
+// TODO: change strategy to use Market to Book ratio to identify arbitrage opportunities
+//TODO: Use yahoo API again once YQL is working correctly
+
 object Main {
+  val ustbill3mo = 1.00 //risk free rate (temporary)
+  val sp5003mo = 2.50 //expected 3mo return on the market
+
   def main(args: Array[String]): Unit = {
+    // Screening part, apply CAPM to filter stock plays
     val topGainerData = Source.fromFile("data/stock.csv")
-    for(line <- topGainerData.getLines()) {
+    for(line <- topGainerData.getLines().drop(1)) {
       val cols = line.split(',').map(_.trim) // trim all leading/trailing whitespace
-      println(cols(0))
+      // Compare analyst estimate to required return
+      val stockTicker = cols(0)
+      val beta = cols(1).toDouble
+      val analystEst = cols(2).toDouble
+      val reqReturn = capm(ustbill3mo, beta, sp5003mo)
+      if (analystEst >= reqReturn) {
+        println("Arbitrage opportunity for " + stockTicker)
+        println("Expected return of %"  + analystEst + " is greater than required return of %" + reqReturn)
+      }
     }
     topGainerData.close()
   }
+
+  // Capital Asset Pricing Model formula
+  def capm(rf:Double,b:Double,rm:Double):Double = rf+(b*(rm-rf))
 }
